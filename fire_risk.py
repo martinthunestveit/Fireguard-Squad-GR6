91655864acb2a346607618a2fd4ff125041c9e02
martinthunestveit <martin.thunestveit@gmail.com>
2026-02-10 15:58:10 +0100

    Testing the testing!

--- fire_risk.py
+++ fire_risk.py
@@ -4,4 +4,11 @@ def get_ttf():
     """Return a random time-to-fire value in minutes."""
     return random.randint(0, 30)
 
-#Change to test the testingsss
+def get_traffic_light(ttf):
+    """Return traffic light color based on time-to-fire."""
+    if ttf < 5:
+        return "red"
+    elif 5 <= ttf <= 15:
+        return "yellow"
+    else:
+        return "green"
